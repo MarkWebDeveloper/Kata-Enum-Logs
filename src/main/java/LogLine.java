fde1df21957df5bd98a8825100b0ba3a4c015989
Mark Kil <markkilcanarias@gmail.com>
2024-04-11 09:23:02 +0200

    first commit

---
+++ src/main/java/LogLine.java
@@ -0,0 +1,21 @@
+
+
+public class LogLine {
+
+    private String logLine;
+
+    public LogLine(String logLine) {
+        this.logLine = logLine;
+    }
+
+    public LogLevel getLogLevel() {
+        return null;
+
+    }
+
+    public String getOutputForShortLog() {
+        return logLine;
+
+    }
+
+}
